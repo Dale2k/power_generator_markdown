98ab1f3c0ece42bf17b03ab78c494972dddca7b1
Darryl Subal <dsubal@att.net>
2023-04-11 19:08:31 -0400

    badge function added

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let badge;
+  if (license !== "none") {
+    badge = `![license badge](https://img.shields/badge/license-${license}-success)`;
+  } else {
+    badge = "";
+  }
+  return badge;
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
