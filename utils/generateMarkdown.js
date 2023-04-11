f15a5bc7d4eeb5357c252a4543e17a5ca3fa78fa
Darryl Subal <dsubal@att.net>
2023-04-11 19:16:47 -0400

    update render license section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,7 +31,17 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  let licenseSection = "";
+  if (license !== "none") {
+    licenseSection = `## License
+The Application is covered under ${license}  license
+Learn more about the license from the following link: ${renderLicenseLink(
+      license
+    )}`;
+  }
+  return licenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
