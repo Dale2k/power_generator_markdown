0b2aa88530a1171416b70aff73ecb0241b620744
Darryl Subal <dsubal@att.net>
2023-04-11 19:15:06 -0400

    return license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,22 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let link;
+  if (license === "MIT") {
+    link =
+      "[https://choosealicense.com/licenses/mit](https://choosealicense.com/licenses/mit)";
+  } else if (license === "Apache") {
+    link =
+      "[https://choosealicense.com/licenses/apache-2.0/](https://choosealicense.com/licenses/apache-2.0/)";
+  } else if (license === "ISC") {
+    link =
+      "[https://choosealicense.com/licenses/](https://choosealicense.com/licenses/isc)";
+  } else {
+    link = "";
+  }
+  return link;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
